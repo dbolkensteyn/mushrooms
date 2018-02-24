799fbc78eb9c41710e0f6cf9b1f1c2c5f8b6602a
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2018-02-24 16:39:52 +0100

    Add the calibration logic

---
+++ calibration.py
@@ -0,0 +1,31 @@
+#  560, 150:   0,   0
+#  556, 365:   0, 200
+#  254, 148: 300,   0
+
+class Calibration:
+  def transform(self, x, y):
+    x1 = 558.
+    y1 = 0.
+    x2 = 254.
+    y2 = 300.
+
+    mx = (y2 - y1) / (x2 - x1)
+    bx = y1 - mx * x1
+
+    xn = int(mx*x + bx)
+
+    x1 = 149.
+    y1 = 0.
+    x2 = 365.
+    y2 = 200.
+
+    my = (y2 - y1) / (x2 - x1)
+    by = y1 - my * x1
+
+    yn = int(my*y + by)
+
+    if xn < 0 or yn < 0 or xn > 300 or yn > 200:
+      raise ValueError()
+
+    return [xn, yn]
+
