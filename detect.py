bd243c6ecddf5442437e9186f91ff238ac0ba174
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2018-02-24 12:12:56 +0100

    Good detection of white mushrooms

--- detect.py
+++ detect.py
@@ -20,14 +20,14 @@ _, gray_im = cv2.threshold(im, 130, 255, cv2.THRESH_BINARY)
 gray_im = cv2.erode(gray_im, (3, 3))
 gray_im = auto_canny(gray_im)
 
-circles = cv2.HoughCircles(gray_im, cv2.cv.CV_HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=10, maxRadius=100)
-
-circles = np.uint16(np.around(circles))
-for i in circles[0,:]:
-    # draw the outer circle
-    cv2.circle(im, (i[0],i[1]), i[2], (0,255,0), 2)
-    # draw the center of the circle
-    cv2.circle(im, (i[0],i[1]), 2, (0,0,255), 3)
+circles = cv2.HoughCircles(gray_im, cv2.cv.CV_HOUGH_GRADIENT, 1, minDist=10, param1=50, param2=13, minRadius=7, maxRadius=18)
+if circles != None:
+  circles = np.uint16(np.around(circles))
+  for i in circles[0,:]:
+      # draw the outer circle
+      cv2.circle(im, (i[0],i[1]), i[2], (0,255,0), 2)
+      # draw the center of the circle
+      cv2.circle(im, (i[0],i[1]), 2, (0,0,255), 3)
 
 cv2.imshow("demo", im)
 cv2.waitKey()
